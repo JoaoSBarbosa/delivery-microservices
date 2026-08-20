7eb11ab7b2beabad227216ea012786c4729c6637
João Barbosa <contato.jsbarbosa@gmail.com>
2026-08-20 18:48:40 -0300

    :sparkles: Adiciona microsserviço de courier

---
+++ ms-courier/src/test/java/com/barbosacode/delivery/mscourier/MsCourierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbosacode.delivery.mscourier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCourierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
